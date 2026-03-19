77a042dc85d1f7e63935a318cc123b5e2bd7b5b7
Sarthak Mohapatra <140512881+sarthakm402@users.noreply.github.com>
2026-03-19 11:44:29 +0530

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -18,7 +18,7 @@ tokenizer.pad_token = tokenizer.eos_token
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16
+    bnb_4bit_compute_dtype=torch.float16 
 )
 
 base_model = AutoModelForCausalLM.from_pretrained(
